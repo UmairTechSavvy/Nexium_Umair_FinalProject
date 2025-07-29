0f00e86c6bf66ba133eaf90e1cbea151a42335b6
Umair Hasan <umairumair12!@gmail.com>
2025-07-29 21:45:46 +0500

    ojj

--- src/middleware.js
+++ src/middleware.js
@@ -4,20 +4,19 @@ export function middleware(request) {
   const path = request.nextUrl.pathname;
 
   const publicPath = path === "/login" || path === "/signup";
-
   const token = request.cookies.get("token")?.value || "";
 
   if (publicPath && token) {
+    // Redirect to home page if user is already logged in
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
-
   if (!publicPath && !token) {
-   
+    // Redirect to login if user tries to access private pages without being logged in
     if (path === "/login") {
-      return NextResponse.redirect(new URL("/signup", request.nextUrl));
+      return NextResponse.redirect(new URL("/signup", request.nextUrl)); // if at login page, redirect to signup
     }
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
+    return NextResponse.redirect(new URL("/login", request.nextUrl)); // redirect to login page
   }
 }
 
