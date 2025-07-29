9bb1662aead1decd9f9e40d477eb991f8062479f
Umair Hasan <umairumair12!@gmail.com>
2025-07-29 21:49:30 +0500

    ppo

--- src/middleware.js
+++ src/middleware.js
@@ -2,21 +2,20 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request) {
   const path = request.nextUrl.pathname;
-
   const publicPath = path === "/login" || path === "/signup";
   const token = request.cookies.get("token")?.value || "";
 
+  // Prevent logged-in users from accessing login/signup pages
   if (publicPath && token) {
-    // Redirect to home page if user is already logged in
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
+  // Redirect to login/signup for non-authenticated users trying to access protected pages
   if (!publicPath && !token) {
-    // Redirect to login if user tries to access private pages without being logged in
     if (path === "/login") {
-      return NextResponse.redirect(new URL("/signup", request.nextUrl)); // if at login page, redirect to signup
+      return NextResponse.redirect(new URL("/signup", request.nextUrl)); // Redirect login to signup
     }
-    return NextResponse.redirect(new URL("/login", request.nextUrl)); // redirect to login page
+    return NextResponse.redirect(new URL("/login", request.nextUrl)); // Redirect protected pages to login
   }
 }
 
